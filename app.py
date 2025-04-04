badcef17449cc94774c4736d9915d8366686846a
vishal <vishalt8114@gmail.com>
2025-04-04 20:39:27 +0530

    bk

--- app.py
+++ app.py
@@ -1286,6 +1286,4 @@ def track_ride(ride_id):
     })
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
